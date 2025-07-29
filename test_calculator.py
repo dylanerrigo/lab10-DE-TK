fb57cff168390e10965c0778728b941c5b821347
Dylan Errigo <dylan.errigo@ufl.edu>
2025-07-29 13:55:30 -0400

    did my sections

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,6 @@
+#https://github.com/dylanerrigo/lab10-DE-TK.git
+#Partner 1: Dylan Errigo
+#Partner 2: Tristan Kerr
 import unittest
 from calculator import *
 
@@ -11,13 +14,14 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(0,1),0)
+        self.assertEqual(mul(1,2),2)
+        self.assertEqual(mul(-2,3),-6)
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(2,1),2)
+        self.assertEqual(div(3,-1),-3)
+        self.assertRaises(ZeroDivisionError,div,3,0)
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
     #     # call division function inside, example:
@@ -34,22 +38,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaise(ValueError):
+            log(0,5)
+        self.assertEqual(log(10,10),1)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3,4),5)
+        self.assertEqual(hypotenuse(6,8),10)
+        self.assertEqual(hypotenuse(1,0),1)
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        self.assertEqual(square_root(4),2)
+        self.assertEqual(square_root(0),0)
+        self.assertEqual(square_root(16),4)
 
 # Do not touch this
 if __name__ == "__main__":
