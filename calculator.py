1cafb550aa603b7f348d15726ee1764c716bb2f3
Dylan Errigo <dylan.errigo@ufl.edu>
2025-07-29 12:40:06 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,5 +7,12 @@ One function per operation, in order.
 # First example
 def add(a, b): 
     pass
-
-
+import math
+def div(a,b):
+    if a==0:
+        raise ValueError('Divisor cannot be zero.')
+    return b/a
+def log(a,b):
+    if b==0:
+        raise ValueError('Argument/antilogarithm cannot be zero.')
+    return math.log(b,a)
